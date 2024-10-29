0f432b8936249be18335c991f977925bc2a912e0
Lukas <lkohlfuerst@eurofunk.com>
2024-10-29 12:24:57 +0100

    path changing stuff

--- src/Core/LZ77.java
+++ src/Core/LZ77.java
@@ -3,7 +3,6 @@ package Core;
 import Utils.File;
 import Utils.Messages;
 
-
 public class LZ77 extends File {
 
     public void run(String[] args) {
@@ -12,26 +11,26 @@ public class LZ77 extends File {
             return;
         }
 
-        if (args.length == 1) {
+        if (args.length < 2) {
             System.out.println(Messages.specifyPath);
             return;
         }
 
         byte[] fileData = File.readFile(args[1]);
-        if (fileData == null) {
+        if (fileData == null || fileData.length == 0) {
+            System.out.println("Error: Could not read file or file is empty.");
             return;
         }
-
 
         switch (args[0]) {
             case "-c":
             case "--compress":
-                Compression.compress(fileData, "Files/output");
+                Compression.compress(fileData, args[args.length - 1].replace(".txt", "") + ".lz");
                 break;
 
             case "-d":
             case "--decompress":
-                Decompression.decompress(fileData, "Files/decompressed.txt");
+                Decompression.decompress(fileData, args[args.length - 1].replace(".lz", "") + ".txt");
                 break;
 
             default:
